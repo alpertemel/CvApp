54fe828db005929ab4b8ed546e7da2fc1c12bcc8
Alper Temel <a.alpertemel@gmail.com>
2020-06-13 02:15:01 +0300

    Photo sayfası eklendi
    Firebase bağlantısı yapıldı. Photo sayfası oluşturuldu. Anasayfadan photo sayfasına geçiş yapılması için buton eklendi.

---
+++ app/src/main/java/com/example/cvapp/photo.java
@@ -0,0 +1,45 @@
+package com.example.cvapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.firebase.storage.StorageReference;
+
+public class photo extends AppCompatActivity {
+
+    Button ch, up;
+    ImageView img;
+    StorageReference mStorageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+        ch=(Button)findViewById(R.id.btnchoose);
+        up=(Button)findViewById(R.id.btnupload);
+        img=(ImageView) findViewById(R.id.imgview);
+
+        ch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Filechooser();
+            }
+        });
+
+    }
+    private void Filechooser(){
+
+        Intent intent = new Intent();
+        intent.setType("image/' ");
+        intent.setAction((Intent.ACTION_GET_CONTENT));
+        startActivityForResult(intent,requestCode 1);
+    }
+
+}
